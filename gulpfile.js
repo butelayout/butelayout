12b6718af6dd2b545705af3f199bee38ffcbb5e3
butelayout <35915935+butelayout@users.noreply.github.com>
2018-01-30 15:47:49 +0200

    Init

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var sass = require('gulp-sass');
+
+gulp.task('css', function () {
+  function callback(file) {
+    return {
+      plugins: [
+        require('css-mqpacker'),
+        require('cssnano')]
+    };
+  }
+  return gulp
+    .src('./src/butelayout.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss(callback))
+    .pipe(gulp.dest('./dist'));
+});
+
+
+gulp.task('watch', function () {
+  gulp.watch('./src/**/*.*', gulp.series('css'));
+});
